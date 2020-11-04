a0d1c167aac6a4e6bd15a1c0b831dcec2afc3175
ewyluda <35318463+ewyluda@users.noreply.github.com>
2020-11-04 15:15:39 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -37,5 +37,36 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+DROP TABLE titles;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+  );
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
 
 SELECT * FROM departments;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
